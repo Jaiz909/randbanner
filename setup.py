16d0e45f5111fa8b29dbd0ee9b7f3367ec1d8955
Jai Grimshaw <jai@jaigrimshaw.com>
2015-08-01 00:31:50 -0400

    Add homepage to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='randbanner',
       version='1.0',
       description='Random banner generator with colour support.',
       author='Jai Grimshaw',
+      url='https://github.com/Jaiz909/randbanner/',
       author_email='randbanner@jaigrimshaw.com',
       license='MIT',
       install_requires=[
